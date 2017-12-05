77f6c795ada2353d3ddc705f11e350b9e592c5a7
Jakub Mikulas <jakub@mikul.as>
2017-12-05 16:36:22 +0100

    test: add cypress tests

---
+++ cypress/integration/api_spec.js
@@ -0,0 +1,42 @@
+describe('Ace-diff API', () => {
+  beforeEach(function () {
+    cy.visit('http://localhost:8081/tests/fixtures/');
+  });
+
+  context('getEditors()', () => {
+    it('returns left and right editors', () => {
+      cy.window().then((win) => {
+        expect(win.aceDiffer.getEditors()).to.have.all.keys('left', 'right');
+      });
+    });
+  });
+
+  context('setOptions()', () => {
+    it('lets you set the ace-diff options', () => {
+      cy.window().then((win) => {
+        expect(win.aceDiffer.options.diffGranularity).to.equal('broad');
+        win.aceDiffer.setOptions({
+          diffGranularity: 'specific'
+        });
+        expect(win.aceDiffer.options.diffGranularity).to.equal('specific');
+      });
+    });
+  });
+
+  context('getNumDiffs()', () => {
+    it('gets you the number of diffs', () => {
+      cy.window().then((win) => {
+        expect(win.aceDiffer.getNumDiffs()).to.equal(2);
+      });
+    });
+  });
+
+  context('diff()', () => {
+    // TODO: is there a good way to test this?
+    it('updates the diff', () => {
+      cy.window().then((win) => {
+        expect(win.aceDiffer.diff()).to.equal(undefined);
+      });
+    });
+  });
+});
