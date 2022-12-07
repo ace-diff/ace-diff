f840da1373ef532f5acd70d8f04b8dd2da33c812
Jakub Mikulas <jakub@mikul.as>
2022-12-07 20:09:58 +0100

    test: update cypress and jest frameworks

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -1,6 +1,8 @@
 describe('Ace-diff API', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8081/cypress-index.html');
+    cy.get('.acediff__wrap').should('have.length', 1);
+    cy.wait(100);
   });
 
   context('getEditors()', () => {
--- cypress/e2e/merging.cy.js
+++ cypress/e2e/merging.cy.js
@@ -1,23 +1,23 @@
 describe('Merging code', () => {
   context('Merging new code', () => {
-    before(() => {
+    beforeEach(() => {
       cy.visit('http://localhost:8081/cypress-index.html');
+      cy.get('.acediff__wrap').should('have.length', 1);
+      cy.wait(100);
     });
 
-    it('shows different version and date in editors', () => {
+    it('shows different version and date in editors and shows 1 new code arrow', () => {
       cy.window().then((win) => {
         const leftCode = JSON.parse(win.aceDiffer.getEditors().left.getValue());
         const rightCode = JSON.parse(win.aceDiffer.getEditors().right.getValue());
         expect(leftCode.version).to.not.equal(rightCode.version);
         expect(leftCode.date).to.not.equal(rightCode.date);
       });
-    });
 
-    it('shows 1 new code arrow', () => {
       cy.get('.acediff__newCodeConnector').should('have.length', 1);
     });
 
-    it('merges code from right to left', () => {
+    it('merges code from right to left and shows no new code arrow', () => {
       cy.get('.acediff__newCodeConnector').click();
 
       cy.window().then((win) => {
@@ -26,27 +26,25 @@ describe('Merging code', () => {
         expect(leftCode.version).to.equal(rightCode.version);
         expect(leftCode.date).to.equal(rightCode.date);
       });
-    });
 
-    it('shows no new code arrow', () => {
       cy.get('.acediff__newCodeConnector').should('not.exist');
     });
   });
 
   context('Merging deleted code', () => {
-    before(() => {
+    beforeEach(() => {
       cy.visit('http://localhost:8081/cypress-index.html');
+      cy.get('.acediff__wrap').should('have.length', 1);
+      cy.wait(100);
     });
 
-    it('shows different version and date in editors', () => {
+    it('shows different version and date in editors and shows 2 deleted code arrows', () => {
       cy.window().then((win) => {
         const leftCode = JSON.parse(win.aceDiffer.getEditors().left.getValue());
         const rightCode = JSON.parse(win.aceDiffer.getEditors().right.getValue());
         expect(rightCode.keywords).to.not.equal(leftCode.keywords);
       });
-    });
 
-    it('shows 2 deleted code arrows', () => {
       cy.get('.acediff__deletedCodeConnector').should('have.length', 2);
     });
 
