dc247993a4bca21f19c74618972a2558aa041a42
Jason Thomas <jason@openc3.com>
2025-05-27 18:47:56 -0600

    Configure jest and cypress

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
   moduleFileExtensions: ["js"],
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
 };
