bdff4ccda9fe2a585bf8bd731fbd0f0102bd8d57
Emily Ragan <emily@openc3.com>
2026-01-23 16:55:37 -0700

    Remove unused Jest config

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.js$": "babel-jest",
-  },
-  moduleFileExtensions: ["js"],
-  setupFilesAfterEnv: ["@testing-library/jest-dom"],
-};
